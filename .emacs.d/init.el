b69c5a347156d72cea9ae6c5f31d2e70bf98832d
imtayadeway <imtayadeway@users.noreply.github.com>
2014-02-17 18:20:39 -0500

    created emacs init
    includes some settings for ruby and color themes

---
+++ .emacs.d/init.el
@@ -0,0 +1,12 @@
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+             '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+(add-to-list 'load-path "~/.emacs.d/themes/")
+(require 'color-theme)
+(require 'color-theme-choco)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-choco)))
