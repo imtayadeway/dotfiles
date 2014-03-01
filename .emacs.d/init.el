38ca52a1284281ee5412b97fc13babe3d7c76d4b
Tim Wade <twade@peachtreebilling.com>
2014-03-01 18:52:11 -0500

    put autosave in temp dir

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,3 +10,8 @@
   '(progn
      (color-theme-initialize)
      (color-theme-choco)))
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
