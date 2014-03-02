ac6d2fa8ba773e54927d3a5a50f8fa73e88b3e46
Tim Wade <twade@peachtreebilling.com>
2014-03-01 19:46:24 -0500

    delete trailing whitespace, load snippets, put autosaves in temp dir

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,10 @@
+;; ruby mode
 (add-to-list 'auto-mode-alist
              '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist
              '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
+;; color-theme and choco
 (add-to-list 'load-path "~/.emacs.d/themes/")
 (require 'color-theme)
 (require 'color-theme-choco)
@@ -11,6 +13,16 @@
      (color-theme-initialize)
      (color-theme-choco)))
 
+;; delete trailing white space
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; load snippets
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; put autosaves in temp dir
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
