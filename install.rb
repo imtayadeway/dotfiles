a83df1045b400ae593a52feec238da09f52b2333
Tim Wade <twade@peachtreebilling.com>
2014-03-19 12:19:37 -0400

    add install script

---
+++ install.rb
@@ -0,0 +1,14 @@
+files = [
+  '.emacs.d',
+  '.gitconfig',
+  '.rspec'
+]
+
+files.each do |file|
+  target_file = File.expand_path("~/#{ file }")
+  source_file = File.expand_path("~/.dotfiles/#{ file }")
+
+  unless File.exists?(target_file) || File.symlink?(target_file)
+    File.symlink(source_file, target_file)
+  end
+end
