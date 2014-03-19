59ed6accfa530fdff306e26e14f519955b58731e
Tim Wade <twade@peachtreebilling.com>
2014-03-19 16:20:07 -0400

    Add coffee-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -67,3 +67,15 @@
 (add-to-list 'load-path "~/.emacs.d/modes/scss-mode")
 (require 'scss-mode)
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+
+;; coffee-mode
+(add-to-list 'load-path "~/.emacs.d/modes/coffee-mode")
+(require 'coffee-mode)
+(custom-set-variables
+ '(coffee-tab-width 2)
+ '(coffee-args-compile '("-c" "--bare")))
+
+(eval-after-load "coffee-mode"
+  '(progn
+     (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+     (define-key coffee-mode-map (kbd "C-j") 'coffee-newline-and-indent)))
