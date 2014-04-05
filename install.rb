89117fb79f238c9205a1a6fa3ab7d7b94063c5ca
Tim Wade <twade@peachtreebilling.com>
2014-04-05 09:18:57 -0400

    add notification

--- install.rb
+++ install.rb
@@ -9,6 +9,8 @@ files.each do |file|
   target_file = File.expand_path("~/#{ file }")
   source_file = File.expand_path("~/.dotfiles/#{ file }")
 
+  puts "skipping #{ target_file }" if File.exists?(target_file)
+
   unless File.exists?(target_file) || File.symlink?(target_file)
     File.symlink(source_file, target_file)
   end
