805d39601528f62d517a69efb7593596f0f9f3fe
Tim Wade <twade@peachtreebilling.com>
2014-04-24 15:49:27 -0400

    Reorganize init.d, disable mouse interface, auto refresh

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,30 +1,14 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 ;; welcome screen
 (setq inhibit-startup-message t)
 
 ;; full screen
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
-
-;; toolbar
-(tool-bar-mode -1)
-
-;; whitespace
-(require 'whitespace)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
-(global-whitespace-mode t)
-
-;; pair character completion
-(electric-pair-mode 1)
-
-;; rainbow mode
-(add-to-list 'load-path "~/.emacs.d/modes/rainbow-mode")
-(require 'rainbow-mode)
-
-;; ruby mode
-(add-to-list 'auto-mode-alist
-             '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist
-             '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
 ;; color-theme and choco
 (add-to-list 'load-path "~/.emacs.d/themes/choco")
@@ -35,8 +19,27 @@
      (color-theme-initialize)
      (color-theme-choco)))
 
-;; delete trailing white space
+;; put autosaves in temp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+;; whitespace
+(require 'whitespace)
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+(global-whitespace-mode t)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; pair character completion
+(electric-pair-mode 1)
 
 ;; load snippets
 (add-to-list 'load-path
@@ -44,11 +47,24 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-;; put autosaves in temp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+;; multi-term
+(add-to-list 'load-path "~/.emacs.d/elisp/external/multi-term")
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
+(setq multi-term-program-switches "--login")
+(add-hook 'term-mode-hook
+          (lambda()
+            (setq yas-dont-activate t)))
+
+;; rainbow mode
+(add-to-list 'load-path "~/.emacs.d/modes/rainbow-mode")
+(require 'rainbow-mode)
+
+;; ruby mode
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+             '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
 ;; line numbers
 (global-linum-mode 1)
