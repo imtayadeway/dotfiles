44fe4fa8e7ddf0377bf091b2efaaaaa8eaa1ac74
Tim Wade <twade@peachtreebilling.com>
2014-06-13 09:27:12 -0400

    Add multi-term and magit keybindings.

--- .emacs.d/lisp/keybindings.el
+++ .emacs.d/lisp/keybindings.el
@@ -1,2 +1,4 @@
 (global-set-key (kbd "M-<up>") 'move-line-up)
 (global-set-key (kbd "M-<down>") 'move-line-down)
+(global-set-key (kbd "C-c s") 'multi-term)
+(global-set-key (kbd "C-x g") 'magit-status)
