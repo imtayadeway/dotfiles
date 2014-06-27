88c775093ec36ed2d8eacedde8a129518c077250
Tim Wade <twade@peachtreebilling.com>
2014-06-27 12:04:16 -0400

    Add misc utils and keybindings

--- .emacs.d/lisp/keybindings.el
+++ .emacs.d/lisp/keybindings.el
@@ -2,3 +2,10 @@
 (global-set-key (kbd "M-<down>") 'move-line-down)
 (global-set-key (kbd "C-c s") 'multi-term)
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-M-\\") 'tidy-region)
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "M-#") 'sort-lines)
+
+;; switch to new window on split
+(global-set-key (kbd "C-x 2") 'split-window-below-and-switch)
+(global-set-key (kbd "C-x 3") 'split-window-right-and-switch)
--- .emacs.d/lisp/utils.el
+++ .emacs.d/lisp/utils.el
@@ -8,3 +8,42 @@
   (forward-line 1)
   (transpose-lines 1)
   (forward-line -1))
+
+(defun tidy-region (start end)
+  "Indent, delete whitespace, and untabify the region."
+  (interactive "r")
+  (progn
+    (delete-trailing-whitespace start end)
+    (indent-region start end nil)
+    (untabify start end)))
+
+(defun tidy-buffer ()
+  "Indent, delete whitespace, and untabify the buffer."
+  (interactive)
+  (save-excursion
+    (tidy-region (point-min) (point-max))))
+
+(defun split-window-below-and-switch ()
+  (interactive)
+  (split-window-below)
+  (other-window 1))
+
+(defun split-window-right-and-switch ()
+  (interactive)
+  (split-window-right)
+  (other-window 1))
+
+(defun comment-or-uncomment-region-or-line ()
+  "Comments or uncomments the region or the current line if there's no active region."
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+        (setq beg (region-beginning) end (region-end))
+      (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)))
+
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
