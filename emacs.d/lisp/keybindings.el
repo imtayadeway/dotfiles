d8dd17742dcb7049cb5e545e7a2da1e6f2f06325
imtayadeway <imtay.wade@gmail.com>
2014-08-18 21:26:17 -0400

    Add keybinding for ace jump mode.

--- emacs.d/lisp/keybindings.el
+++ emacs.d/lisp/keybindings.el
@@ -4,6 +4,7 @@
 (global-set-key (kbd "C-c m") 'mu4e)
 (global-set-key (kbd "C-c d") 'date)
 (global-set-key (kbd "C-c t") 'time)
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-M-\\") 'tidy-region)
 (global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
