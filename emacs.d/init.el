9e0fbdc558af5a2ba4b9a68215004d7e2566a48e
Tim Wade <timwade@Tims-MacBook-Pro.local>
2014-10-06 10:11:38 -0700

    Turn off auto refresh non-file buffers.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,12 +20,12 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; ;; Auto refresh buffers
+;; Auto refresh buffers
 (global-auto-revert-mode 1)
 
-;; ;; Also auto refresh dired, but be quiet about it
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
+;; Also auto refresh dired, but be quiet about it
+;; (setq global-auto-revert-non-file-buffers t)
+;; (setq auto-revert-verbose nil)
 
 ;; pallet
 (require 'cask "~/.cask/cask.el")
