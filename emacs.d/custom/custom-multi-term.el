76997e15087f32dbb76f6bf67e2f358643c146ff
Tim Wade <tim@majority.co>
2015-01-30 12:32:58 -0800

    Turn linum off in multi-term.

--- emacs.d/custom/custom-multi-term.el
+++ emacs.d/custom/custom-multi-term.el
@@ -5,4 +5,5 @@
 (lambda ()
             (define-key term-raw-map (kbd "M-o") 'other-window)
             (define-key term-raw-map (kbd "C-y") 'term-paste)
-            (setq yas-dont-activate t)))
+            (setq yas-dont-activate t)
+            (linum-mode -1)))
