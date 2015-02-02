75047f403af03465f70006efb0973586bc33c7ed
Tim Wade <tim@majority.co>
2015-02-02 15:47:32 -0800

    Put main at the top.

--- install.rb
+++ install.rb
@@ -1,3 +1,18 @@
+def main
+  if File.exist?(File.expand_path('~/.bash_profile'))
+    File.delete(File.expand_path('~/.bash_profile'))
+  end
+
+  files.each do |file|
+    if File.exist?(target(file))
+      print "skipping #{ target(file) }: #{ skip_reason(target(file)) }"
+    else
+      puts "linking #{ target(file) }"
+      File.symlink(source(file), target(file))
+    end
+  end
+end
+
 def files
   [
     "ackrc",
@@ -32,21 +47,6 @@ def skip_reason(file)
     "already linked"
   else
     "file already exists. delete or move before reinstalling."
-  end
-end
-
-def main
-  if File.exist?(File.expand_path('~/.bash_profile'))
-    File.delete(File.expand_path('~/.bash_profile'))
-  end
-
-  files.each do |file|
-    if File.exist?(target(file))
-      print "skipping #{ target(file) }: #{ skip_reason(target(file)) }"
-    else
-      puts "linking #{ target(file) }"
-      File.symlink(source(file), target(file))
-    end
   end
 end
 
