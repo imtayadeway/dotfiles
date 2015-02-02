871f33023118ead5ecd90032b20ee0e2c3c833b7
Tim Wade <tim@majority.co>
2015-02-02 15:44:07 -0800

    Use more functional/imperative style in install

--- install.rb
+++ install.rb
@@ -1,24 +1,22 @@
-files = [
-  'ackrc',
-  'agignore',
-  'bash_profile',
-  'bashrc',
-  'bashrc.aliases',
-  'bashrc.prompt',
-  'bin',
-  'emacs.d',
-  'gemrc',
-  'gitconfig',
-  'gitignore',
-  'gitmessage',
-  'offlineimaprc',
-  'offlineimap.py',
-  'rspec',
-  'vimrc',
-]
-
-if File.exist?(File.expand_path('~/.bash_profile'))
-  File.delete(File.expand_path('~/.bash_profile'))
+def files
+  [
+    "ackrc",
+    "agignore",
+    "bash_profile",
+    "bashrc",
+    "bashrc.aliases",
+    "bashrc.prompt",
+    "bin",
+    "emacs.d",
+    "gemrc",
+    "gitconfig",
+    "gitignore",
+    "gitmessage",
+    "offlineimaprc",
+    "offlineimap.py",
+    "rspec",
+    "vimrc",
+  ]
 end
 
 def source(file)
@@ -29,16 +27,27 @@ def target(file)
   File.expand_path("~/.dotfiles/#{ file }")
 end
 
-files.each do |file|
-  if File.exist?(target(file))
-    print "skipping #{ target(file) }: "
-    if File.symlink?(target(file))
-      puts "already linked"
-    else
-      puts "file already exists. delete or move before reinstalling."
-    end
+def skip_reason(file)
+  if File.symlink?(file)
+    "already linked"
   else
-    puts "linking #{ target(file) }"
-    File.symlink(source_file, target_file)
+    "file already exists. delete or move before reinstalling."
   end
 end
+
+def main
+  if File.exist?(File.expand_path('~/.bash_profile'))
+    File.delete(File.expand_path('~/.bash_profile'))
+  end
+
+  files.each do |file|
+    if File.exist?(target(file))
+      print "skipping #{ target(file) }: #{ skip_reason(target(file)) }"
+    else
+      puts "linking #{ target(file) }"
+      File.symlink(source(file), target(file))
+    end
+  end
+end
+
+main
