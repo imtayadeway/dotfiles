278f3f9947e4ddeec50347c3181cc6a75808c271
Tim Wade <tim@majority.co>
2015-02-20 15:37:22 -0800

    Use utf-8 in term.

--- emacs.d/custom/custom-multi-term.el
+++ emacs.d/custom/custom-multi-term.el
@@ -2,8 +2,13 @@
 (setq multi-term-program-switches "--login")
 
 (add-hook 'term-mode-hook
-(lambda ()
+          (lambda ()
             (define-key term-raw-map (kbd "M-o") 'other-window)
             (define-key term-raw-map (kbd "C-y") 'term-paste)
             (setq yas-dont-activate t)
             (linum-mode -1)))
+
+(add-hook 'term-exec-hook
+          (function
+           (lambda ()
+             (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
