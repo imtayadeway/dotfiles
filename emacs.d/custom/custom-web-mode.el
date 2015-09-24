69ecc15b07a33bf224c1fce46a141172114d969f
Tim Wade <hello@timjwade.com>
2015-09-24 09:16:08 -0700

    Indent two spaces in web-mode

--- emacs.d/custom/custom-web-mode.el
+++ emacs.d/custom/custom-web-mode.el
@@ -1,3 +1,6 @@
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.rhtml$\\'" . web-mode))
