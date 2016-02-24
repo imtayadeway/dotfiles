ca276eaa88b2d5ff80908eb81e267210c932de2e
Tim Wade <hello@timjwade.com>
2016-02-23 21:17:07 -0800

    Customise diminish a bit more

--- emacs.d/lisp/hide-modes.el
+++ emacs.d/lisp/hide-modes.el
@@ -6,10 +6,16 @@
   `(add-hook ,mode-hook
              (lambda () (setq mode-name ,abbrev))))
 
+(diminish-minor-mode 'eldoc 'eldoc-mode)
 (diminish-minor-mode 'flycheck 'flycheck-mode)
+(diminish-minor-mode 'flyspell 'flyspell-mode)
+(diminish-minor-mode 'global-whitespace 'global-whitespace-mode)
 (diminish-minor-mode 'projectile 'projectile-mode)
+(diminish-minor-mode 'ruby-end 'ruby-end-mode)
 (diminish-minor-mode 'yasnippet 'yas-minor-mode)
 (diminish-minor-mode 'whitespace 'whitespace-mode)
+(diminish-minor-mode 'wrap-region 'wrap-region-mode)
 
 (diminish-major-mode 'emacs-lisp-mode-hook "el")
 (diminish-major-mode 'lisp-interaction-mode-hook "λ")
+(diminish-major-mode 'python-mode-hook "Py")
