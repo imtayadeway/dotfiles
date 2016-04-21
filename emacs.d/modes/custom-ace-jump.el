622b128a8fead4e43b7334bb68357d34ce34c126
Tim Wade <hello@timjwade.com>
2016-04-20 22:03:36 -0700

    Prefer avy over ace jump

--- emacs.d/modes/custom-ace-jump.el
+++
@@ -1,2 +0,0 @@
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-(setq ace-jump-mode-scope 'window)
---
+++ emacs.d/modes/custom-avy.el
@@ -0,0 +1,4 @@
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(setq avy-style 'at)
+(setq avy-background t)
+(setq avy-all-windows nil)
