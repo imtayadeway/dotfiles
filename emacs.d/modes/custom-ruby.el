21c041db9db730578aa87d64564f678c6bf0567e
Tim Wade <hello@timjwade.com>
2016-04-20 22:05:24 -0700

    Don't do aggressive indent for ruby

--- emacs.d/modes/custom-ruby.el
+++ emacs.d/modes/custom-ruby.el
@@ -8,7 +8,6 @@
             (ruby-end-mode)
             (flycheck-mode)
             (rubocop-mode)
-            (aggressive-indent-mode)
             (global-set-key (kbd "C-c h") 'ruby-toggle-hash-syntax)
             (local-set-key "\r" 'newline-and-indent)
             (define-key ruby-mode-map (kbd "C-c C-c") 'xmp)))
