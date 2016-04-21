c903c501a448c0f52a4bf22c9238433617f59a79
Tim Wade <hello@timjwade.com>
2016-04-20 22:36:52 -0700

    Smex

--- emacs.d/lisp/keybindings.el
+++ emacs.d/lisp/keybindings.el
@@ -37,3 +37,8 @@
 
 ;; toggle quotes
 (global-set-key (kbd "C-M-'") 'toggle-quotes)
+
+;; smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;; old M-x
