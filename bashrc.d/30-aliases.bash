deecce73bf2fae6d9ef9ad3600176ee83be7b910
Tim Wade <hello@timjwade.com>
2016-05-04 15:55:44 -0700

    Group directories with ls

--- bashrc.d/30-aliases.bash
+++ bashrc.d/30-aliases.bash
@@ -72,10 +72,10 @@ alias js="bundle exec jekyll serve --drafts"
 alias jsonpp="python -m json.tool"
 alias killemacs="emacsclient -e '(kill-emacs)'"
 alias killruby='killall -9 ruby'
-alias la="ls -la"
+alias la="ls -la --group-directories-first"
 alias less="less -R" # display colors correctly
-alias lh="ls -lah"
-alias ll="ls -l"
+alias lh="ls -lahv --group-directories-first"
+alias ll="ls -l --group-directories-first"
 alias ln="ln -v"
 
 if [[ $(uname) == Darwin ]]; then
