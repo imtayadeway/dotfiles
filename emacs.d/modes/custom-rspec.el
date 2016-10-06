785cfe1c829412dafedec64d6c8778a25c9635ad
Tim Wade <hello@timjwade.com>
2016-10-05 20:49:47 -0700

    Don't try to use zeus or spring

--- emacs.d/modes/custom-rspec.el
+++ emacs.d/modes/custom-rspec.el
@@ -1,3 +1,5 @@
 (setq rspec-use-rake-when-possible nil)
 (setq rspec-use-opts-file-when-available nil)
+(setq rspec-use-zeus-when-possible nil)
+(setq rspec-use-spring-when-possible nil)
 (setq rspec-command-options "--format progress")
