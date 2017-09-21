d31f8651b7e1527ef0b84c0e976b4d7e1f4c3dd3
Tim Wade <hello@timjwade.com>
2017-09-21 09:41:24 -0700

    Fontify codeblocks in markdown

--- emacs.d/modes/custom-markdown.el
+++ emacs.d/modes/custom-markdown.el
@@ -1,6 +1,8 @@
 (setq auto-mode-alist
       (cons '("\\.md$" . markdown-mode) auto-mode-alist))
 
+(setq markdown-fontify-code-blocks-natively t)
+
 (add-hook 'markdown-mode-hook
 (lambda ()
             (flyspell-mode)))
