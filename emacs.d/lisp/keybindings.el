f3e5539ab9903257416b2d8460fbab36ca4a1c43
Tim Wade <hello@timjwade.com>
2017-09-21 09:41:44 -0700

    Toggle evil mode with f4

--- emacs.d/lisp/keybindings.el
+++ emacs.d/lisp/keybindings.el
@@ -42,3 +42,6 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;; old M-x
+
+;; evil
+(global-set-key (kbd "<f4>") 'evil-mode)
