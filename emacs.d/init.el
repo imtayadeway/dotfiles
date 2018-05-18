e846c805fe1892881ca4383739595f4a685f49bb
Tim Wade <hello@timjwade.com>
2018-05-18 08:35:14 -0700

    Emacs auto generated stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,6 +6,10 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
@@ -145,3 +149,9 @@
   (switch-to-buffer "*terminal<1>*")
   (other-window 1)
   (mu4e))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
