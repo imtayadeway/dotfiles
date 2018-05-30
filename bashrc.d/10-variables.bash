d386b68d27a22e67a06b2110d775eee24963bd45
Tim Wade <hello@timjwade.com>
2018-05-30 09:26:25 -0700

    OMG don't set PGUSER to root

--- bashrc.d/10-variables.bash
+++ bashrc.d/10-variables.bash
@@ -8,7 +8,6 @@ export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 export HISTFILESIZE=20000
 export TERM="xterm-256color"
-export PGUSER=root
 PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "
 
 source "/usr/local/share/chruby/chruby.sh"
