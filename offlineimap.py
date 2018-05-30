d2391b7be8c422e56d430fe2dac4ffd58c4fac8e
Tim Wade <hello@timjwade.com>
2018-05-30 09:29:19 -0700

    Tell offlineimap to use gpg2 in case there's a conflict

--- offlineimap.py
+++ offlineimap.py
@@ -3,5 +3,5 @@ import os, re
 def get_authinfo_password(machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
-    authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
+    authinfo = os.popen("gpg2 -q --no-tty -d ~/.authinfo.gpg").read()
     return p.search(authinfo).group(1)
