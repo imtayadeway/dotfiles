065fcfd23cdfd1a81cbcd027133565490add1c3a
Tim Wade <hello@timjwade.com>
2018-07-11 10:34:22 -0700

    Make some complicated aliases functions instead

--- bashrc.d/50-utils.bash
+++ bashrc.d/50-utils.bash
@@ -1,3 +1,26 @@
 function mcd() {
   mkdir -p "$1" && cd "$1";
 }
+
+function myip() {
+    for interface in $(ifconfig -s | grep BMRU | cut --delimiter=' ' --fields=1)
+    do
+        ifconfig $interface |
+            grep 'inet ' |
+            tr -s ' ' |
+            cut --delimiter=' ' --fields=3 |
+            sed s/addr://
+    done
+}
+
+function oports() {
+    echo 'User:      Command:   Port:'
+    echo '----------------------------'
+    lsof -i 4 -P -n |
+        grep -i 'listen' |
+        awk '{print $3, $1, $9}' |
+        sed 's/ [a-z0-9\.\*]*:/ /' |
+        sort -k 3 -n |
+        xargs printf '%-10s %-10s %-10s\n' |
+        uniq
+}
