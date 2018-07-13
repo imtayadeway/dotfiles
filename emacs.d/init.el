46c9e5719a56893bf23d89a3f3204fdde7668c98
Tim Wade <hello@timjwade.com>
2018-07-13 16:42:12 -0700

    It turns out I don't need this
    Emacs now starts in fullscreen mode, it seems

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,15 +5,6 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages
-   (quote
-    (typescript-mode irfc graphql-mode yasnippet yaml-mode writegood-mode wrap-region web-mode toggle-quotes smex scss-mode sass-mode rust-mode ruby-hash-syntax ruby-end ruby-compilation rubocop rspec-mode rhtml-mode restclient rainbow-mode rainbow-delimiters projectile-rails persp-projectile paredit pallet multi-term minitest markdown-mode lorem-ipsum less-css-mode kibit-helper jump json-mode inf-mongo inf-clojure ido-vertical-mode gitignore-mode gitconfig-mode git-timemachine flycheck flx-ido feature-mode expand-region exec-path-from-shell evil ember-mode diminish color-theme coffee-mode cider chruby bundler alchemist aggressive-indent ag ace-window))))
 ;; hack for toggling fullscreen after init
 (run-with-idle-timer 0.1 nil 'toggle-frame-fullscreen)
 
