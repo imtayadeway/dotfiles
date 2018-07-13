94b4a3228372d8e57ecbdc49170d1287e2bd4e8c
Tim Wade <hello@timjwade.com>
2018-07-13 16:44:40 -0700

    Don't need this either

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -138,10 +138,3 @@
   (switch-to-buffer "*Org Agenda*")
   (other-window 1)
   (mu4e))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
