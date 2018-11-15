18e4965113960789e29c43f8fb0ce594a43c2c12
Tim Wade <hello@timjwade.com>
2018-11-15 14:16:42 -0800

    Tweak history size

--- bashrc.d/variables.bash
+++ bashrc.d/variables.bash
@@ -6,7 +6,8 @@ export EDITOR="emacsclient -nw -c -a ''"
 export LANG="en_US"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-export HISTFILESIZE=20000
+export HISTSIZE=-1
+export HISTFILESIZE=200000
 export TERM="xterm-256color"
 PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "
 
