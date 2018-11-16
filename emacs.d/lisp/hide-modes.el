f0e8c0f5099763981cf3cd662846822bd1f87a71
Tim Wade <hello@timjwade.com>
2018-11-15 17:02:51 -0800

    Remove obsolete diminish-minor-mode

--- emacs.d/lisp/hide-modes.el
+++ emacs.d/lisp/hide-modes.el
@@ -1,7 +1,3 @@
-(defmacro diminish-minor-mode (filename mode &optional abbrev)
-  `(eval-after-load (symbol-name ,filename)
-     '(diminish ,mode ,abbrev)))
-
 (defmacro diminish-major-mode (mode-hook abbrev)
   `(add-hook ,mode-hook
              (lambda () (setq mode-name ,abbrev))))
