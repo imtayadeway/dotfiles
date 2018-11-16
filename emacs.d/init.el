eb3451746160e472526d98b5e23278bcbdec49e5
Tim Wade <hello@timjwade.com>
2018-11-15 16:43:14 -0800

    Set ws line column to 80

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -491,7 +491,7 @@
   (setq-default indent-tabs-mode nil)
   (setq whitespace-style '(face empty tabs lines-tail trailing))
   (setq require-final-newline t)
-  (setq whitespace-line-column 120)
+  (setq whitespace-line-column 80)
   (global-whitespace-mode t)
   (add-hook 'before-save-hook 'delete-trailing-whitespace)
   :diminish (whitespace-mode
