fb8ca65f05c9ec311a1132f066d35938b9a8d36f
Tim Wade <hello@timjwade.com>
2018-11-20 16:42:36 -0800

    Fix multiterm custom keys

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -238,13 +238,14 @@
   :ensure nil)
 
 (use-package multi-term
-  :bind (:map term-raw-map
-              ("M-o" . other-window) ;; TODO: fix me
-              ("C-y" . term-paste))
   :init
   (setq multi-term-program "/bin/bash")
   (setq multi-term-program-switches "--login")
   (setq yas-dont-activate t)
+  (add-hook 'term-mode-hook
+            (lambda ()
+              (define-key term-raw-map (kbd "M-o") 'other-window)
+              (define-key term-raw-map (kbd "C-y") 'term-paste)))
   (add-hook 'term-exec-hook
             (function
              (lambda ()
