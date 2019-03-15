fcdd8d6c40a41a108c823cf4e583051bcc8d3330
Tim Wade <hello@timjwade.com>
2019-03-15 09:19:24 -0700

    Trying caching to see if that speeds up find-file

--- emacs.d/packages/projectile-package.el
+++ emacs.d/packages/projectile-package.el
@@ -4,4 +4,5 @@
   (require 'persp-projectile)
   :diminish
   :init
-  (setq projectile-tags-command "ctags-exuberant -Re -f \"%s\" --languages=-javascript --exclude=.git --exclude=tmp --exclude=log"))
+  (setq projectile-tags-command "ctags-exuberant -Re -f \"%s\" --languages=-javascript --exclude=.git --exclude=tmp --exclude=log")
+  (setq projectile-enable-caching t))
