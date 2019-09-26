d95526ed57ed81768443edcc58eaa32803e4f5d9
Tim Wade <hello@timjwade.com>
2019-09-26 11:17:08 -0700

    Diminish yas

--- emacs.d/packages/yasnippet-package.el
+++ emacs.d/packages/yasnippet-package.el
@@ -2,5 +2,5 @@
   :config
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
   (yas-global-mode 1)
-  :diminish
-  :hook ((python-mode ruby-mode rspec-mode) . yas-minor-mode))
+  :hook ((python-mode ruby-mode rspec-mode) . yas-minor-mode)
+  :diminish (yas-minor-mode . "✂️"))
