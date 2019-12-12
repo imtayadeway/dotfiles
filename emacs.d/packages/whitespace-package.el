c7032999d4b70ec39666a92d91167e2100db3304
Tim Wade <hello@timjwade.com>
2019-12-12 10:52:53 -0800

    Update whitespace config

--- emacs.d/packages/whitespace-package.el
+++ emacs.d/packages/whitespace-package.el
@@ -1,9 +1,8 @@
 (use-package whitespace
   :config
   (setq-default indent-tabs-mode nil)
-  (setq whitespace-style '(face empty tabs lines-tail trailing))
+  (setq whitespace-style '(tab-mark))
   (setq require-final-newline t)
-  (setq whitespace-line-column 80)
   (global-whitespace-mode t)
   (add-hook 'before-save-hook 'delete-trailing-whitespace)
   :diminish (whitespace-mode
