5625fc6df9ede29b5838dd266383f5887eb8c60e
Tim Wade <hello@timjwade.com>
2020-06-17 15:21:24 -0700

    Use --pride

--- emacs.d/packages/minitest-package.el
+++ emacs.d/packages/minitest-package.el
@@ -1,4 +1,5 @@
 (use-package minitest
   :init
   (setq minitest-use-zeus-when-possible nil)
-  (setq minitest-keymap-prefix (kbd "C-c .")))
+  (setq minitest-keymap-prefix (kbd "C-c ."))
+  (setq minitest-default-env "TESTOPTS='--pride'"))
