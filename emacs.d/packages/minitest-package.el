7d31b031d18d78090c289a73d00a8c6ea1b4ba61
Tim Wade <hello@timjwade.com>
2020-10-21 14:28:44 -0700

    Always do pride

--- emacs.d/packages/minitest-package.el
+++ emacs.d/packages/minitest-package.el
@@ -2,4 +2,5 @@
   :init
   (setq minitest-use-zeus-when-possible nil)
   (setq minitest-keymap-prefix (kbd "C-c ."))
-  (setq minitest-default-env "TESTOPTS='--pride'"))
+  (setq minitest-default-env "TESTOPTS='--pride'")
+  (setq minitest-default-command '("ruby" "-Ilib:test" "-rbundler/setup" "-rminitest/pride")))
