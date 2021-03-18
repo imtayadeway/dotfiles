8984bfdd8c58edaca4d27cfbb831de59827772c5
Tim Wade <hello@timjwade.com>
2021-03-18 16:13:41 -0700

    Set tab width to 4 in go

--- emacs.d/packages/go-mode-package.el
+++ emacs.d/packages/go-mode-package.el
@@ -1,3 +1,4 @@
 (use-package go-mode
   :init
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'go-mode-hook (lambda () (setq tab-width 4))))
