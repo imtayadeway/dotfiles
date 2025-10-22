031b6d2a79387fdd932d1661d887b1511c4a4638
Tim Wade <hello@timjwade.com>
2025-10-22 15:47:57 -0700

    Update persp-mode config

--- emacs.d/packages/perspective-package.el
+++ emacs.d/packages/perspective-package.el
@@ -2,6 +2,8 @@
   :bind (:map perspective-map ("m" . twj/go-to-main))
   :config
   (persp-mode)
+  :custom
+  (persp-mode-prefix-key (kbd "C-c M-p"))
   :preface
   (defun twj/go-to-main ()
     (interactive)
